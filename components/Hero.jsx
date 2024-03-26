019ab87c7c2c7c778291c098fc7ad2c441fea716
xndrbrgs <aborgescolon@gmail.com>
2024-03-25 22:31:24 -0400

    added scroll trigger

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,26 +7,49 @@ import { useRef } from "react";
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function Hero() {
-  useGSAP(() => {
-    const images = gsap.utils.toArray("#img-wrapper");
+  const namesRef = useRef();
 
-    images.forEach((image) => {
-      console.log(image);
-      gsap.to("#parent-text", {
-        scrollTrigger: {
-          trigger: image,
-          start: "top bottom",
-          end: "bottom center",
-          scrub: true,
-          markers: true,
-        },
-        y: -110,
-      });
+  useGSAP(() => {
+    // const images = gsap.utils.toArray("#img-wrapper");
+
+    // images.forEach((image, i) => {
+    //   ScrollTrigger.create({
+    //     trigger: image,
+    //     start: "top top",
+    //     end: "bottom center",
+    //     markers: true,
+    //   });
+
+    //   let animation = gsap.timeline().to("#names", { yPercent: -110 });
+
+    //   ScrollTrigger.create({
+    //     trigger: image,
+    //     start: "top bottom",
+    //     end: "bottom center",
+    //     animation: animation,
+    //     scrub: true,
+    //   });
+    // });
+
+    gsap.to("#parent-text", {
+      scrollTrigger: {
+        trigger: "#artists",
+        start: "top bottom",
+        end: "bottom bottom",
+        scrub: true,
+      },
+      yPercent: -275,
+    });
+
+    gsap.from("#hero-wrapper", {
+      y: -100,
+      opacity: 0,
+      duration: 0.8,
     });
   }, {});
 
   return (
-    <section className="font-satoshi overflow-hidden">
+    <section className="font-satoshi overflow-hidden" id="hero-container">
       <div
         id="hero-wrapper"
         className="p-[1.5rem] w-screen h-screen fixed flex flex-col justify-between z-20"
@@ -38,10 +61,18 @@ function Hero() {
 
           <div id="wrapper-title" className="overflow-hidden h-[7rem]">
             <div id="parent-text" className="w-full h-full">
-              <h1 className="display font-bold">LINEUP</h1>
-              <h1 className="display font-bold text-pink">DUA LIPA</h1>
-              <h1 className="display font-bold t">DISCLOSURE</h1>
-              <h1 className="display font-bold text-green">JUNGLE</h1>
+              <h1 className="display font-bold" id="names">
+                LINEUP
+              </h1>
+              <h1 className="display font-bold text-pink" id="names">
+                DUA LIPA
+              </h1>
+              <h1 className="display font-bold text-blue" id="names">
+                DISCLOSURE
+              </h1>
+              <h1 className="display font-bold text-green" id="names">
+                JUNGLE
+              </h1>
             </div>
           </div>
         </div>
@@ -58,68 +89,72 @@ function Hero() {
 
           <div id="trigger" className="w-full h-[50vh]"></div>
 
-          <div id="artist-section" className="w-full">
-            <div className="row justify-end">
-              <div className="col-9">
-                <div id="img-wrapper" className="w-full h-[40rem] relative">
-                  <Image
-                    alt="artist photo"
-                    src={"/assets/dua-lipa-blonde-uhdpaper.com-4K-6.2235.webp"}
-                    fill
-                    style={{
-                      objectFit: "cover",
-                    }}
-                  />
-                </div>
-                <div className="uppercase pt-[2rem] text-end h1 font-editorial">
-                  <p>Pyramid Stage - Friday</p>
+          <div id="artists">
+            <div id="artist-section" className="w-full">
+              <div className="row justify-end">
+                <div className="col-9">
+                  <div id="img-wrapper" className="w-full h-[40rem] relative">
+                    <Image
+                      alt="artist photo"
+                      src={
+                        "/assets/dua-lipa-blonde-uhdpaper.com-4K-6.2235.webp"
+                      }
+                      fill
+                      style={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                  <div className="uppercase pt-[2rem] text-end h1 font-editorial">
+                    <p>Pyramid Stage - Friday</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div id="trigger" className="w-full h-[50vh]"></div>
-          <div id="artist-section" className="w-full">
-            <div className="row">
-              <div className="col-6 ml-[8.333%]">
-                <div id="img-wrapper" className="w-full h-[40rem] relative">
-                  <Image
-                    alt="artist photo"
-                    src={"/assets/Disclosure-hero-press-pic@1400x1050.webp"}
-                    fill
-                    style={{
-                      objectFit: "cover",
-                    }}
-                  />
-                </div>
-                <div className="uppercase pt-[2rem] h1 font-editorial">
-                  <p>Pyramid Stage - saturday</p>
+            <div id="trigger" className="w-full h-[50vh]"></div>
+            <div id="artist-section" className="w-full">
+              <div className="row">
+                <div className="col-6 ml-[8.333%]">
+                  <div id="img-wrapper" className="w-full h-[40rem] relative">
+                    <Image
+                      alt="artist photo"
+                      src={"/assets/Disclosure-hero-press-pic@1400x1050.webp"}
+                      fill
+                      style={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                  <div className="uppercase pt-[2rem] h1 font-editorial">
+                    <p>Pyramid Stage - saturday</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div id="trigger" className="w-full h-[50vh]"></div>
-          <div id="artist-section" className="w-full">
-            <div className="row justify-end">
-              <div className="col-5 mr-[8.333%]">
-                <div id="img-wrapper" className="w-full h-[40rem] relative">
-                  <Image
-                    alt="artist photo"
-                    src={"/assets/Jungle-1-1024x650.webp"}
-                    fill
-                    style={{
-                      objectFit: "cover",
-                    }}
-                  />
-                </div>
-                <div className="uppercase pt-[2rem] text-end h1 font-editorial">
-                  <p>Pyramid Stage - sunday</p>
+            <div id="trigger" className="w-full h-[50vh]"></div>
+            <div id="artist-section" className="w-full">
+              <div className="row justify-end">
+                <div className="col-5 mr-[8.333%]">
+                  <div id="img-wrapper" className="w-full h-[40rem] relative">
+                    <Image
+                      alt="artist photo"
+                      src={"/assets/Jungle-1-1024x650.webp"}
+                      fill
+                      style={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                  <div className="uppercase pt-[2rem] text-end h1 font-editorial">
+                    <p>Pyramid Stage - sunday</p>
+                  </div>
                 </div>
               </div>
             </div>
+            <div id="trigger" className="w-full h-[50vh]"></div>
           </div>
-          <div id="trigger" className="w-full h-[50vh]"></div>
         </div>
       </div>
     </section>
